55e2fdc761a35e75024f04852e86c4a7c36fbb73
Tramanh "Vina" Le <tle19@my.whitworth.edu>
2017-01-06 15:52:31 -0800

    Updated 1/6
    Homework 2 - City :: Complete

---
+++ HW2/src/Kid.java
@@ -0,0 +1,17 @@
+/**
+ * DESCRIPTION: Allows input from PersonClass and allows favorite candy to be implemented
+ * @author tle19
+ * 
+ */
+public class Kid extends PersonClass {
+	String faveCandy;
+	
+	public Kid(String newName, int newAge, int newPhone, String newFaveCandy) {
+		// TODO Auto-generated method stub
+		name = newName;
+		age = newAge;
+		phoneNum = newPhone;
+		faveCandy = newFaveCandy;
+	}
+
+}
